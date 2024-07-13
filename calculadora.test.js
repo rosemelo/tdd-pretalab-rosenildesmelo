309a1fc2223aa86b98434e5a504a857e9fb0b630
Rosenildes Melo <rosenildes.melo@g.globo>
2024-07-13 13:13:53 -0300

    completando o desafio

--- calculadora.test.js
+++ calculadora.test.js
@@ -2,20 +2,24 @@ const Calculadora = require('./calculadora')
 
 describe('Calculadora', () => {
   it('Deverá retornar a soma de dois números', () => {
-    //Seu código aqui
-    
+    const calculadora = new calculadora()
+    expect(calculadora.soma(2,2)).toEqual(4)
+
   })
 
   it('Deverá retornar a subtração de dois números', () => {
-    //Seu código aqui
+    const calculadora = new calculadora()
+    expect(calculadora.subtracao(5,3)).toEqual(2)
     
   })
 
   it('Deverá retornar a multiplicação entre dois números', () => {
-    //Seu código aqui
+    const calculadora = new calculadora()
+    expect(calculadora.multiplicacao(2,3)).toEqual(6)
   })
 
   it('Deverá retornar a divisao entre dois números', () => {
-    //Seu código aqui
+    const calculadora = new calculadora()
+    expect(calculadora.soma(6,3)).toEqual(2)
   })
 })
